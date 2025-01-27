a2e7d23c21fac607435c7a5714853a1adb0d8247
Ropafadzo Muridzi <rmuridzi@axissol.com>
2025-01-27 20:03:55 +0200

    Dealing with cloud cors bug (pending)

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,7 +1,25 @@
+import { adminDb } from "@/firebaseAdmin";
+import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
-function ChatToFilePage({ params: { id } }: { params: { id: string } }) {
-  return <div>ChatToFilePage :{id}</div>;
+async function ChatToFilePage({ params: { id } }: { params: { id: string } }) {
+  auth.protect();
+  const { userId } = await auth();
+
+  const ref = await adminDb
+    .collection("users")
+    .doc(userId!)
+    .collection("files")
+    .doc(id)
+    .get();
+
+  const url = ref.data()?.dowloadUrl;
+  return (
+    <div className="grid h-full overflow-hidden lg:grid-cols-5">
+      <div className="col-span-5 overflow-y-auto lg:col-span-2"></div>
+      <div className="col-span-5 overflow-auto border-r-2 bg-gray-100 lg:-order-1 lg:col-span-3 lg:border-indigo-600"></div>
+    </div>
+  );
 }
 
 export default ChatToFilePage;
---
+++ components/PdfView.tsx
@@ -0,0 +1,14 @@
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+import { Docuent, Page, pdfjs } from "react-pdf";
+import { useEffect } from "react";
+import { Button } from "./ui/button";
+import { Loader2Icon, RotateCw, ZoomInIcon, ZoomOutIcon } from "lucide-react";
+
+// gsutils cors set cors.json gs://chat-with-pdf-ai-ef204.appspot.com
+function PdfView({ url }: { url: string }) {
+  return <div></div>;
+}
+
+export default PdfView;
