b6a53620e6271bd6dd739a831fb6c6746fe14d1e
Ropafadzo Muridzi <rmuridzi@axissol.com>
2025-01-28 04:58:00 +0200

    Ui for view pdf done

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,3 +1,4 @@
+import PdfView from "@/components/PdfView";
 import { adminDb } from "@/firebaseAdmin";
 import { auth } from "@clerk/nextjs/server";
 import React from "react";
@@ -5,7 +6,7 @@ import React from "react";
 async function ChatToFilePage({ params: { id } }: { params: { id: string } }) {
   auth.protect();
   const { userId } = await auth();
-
+  console.log(id);
   const ref = await adminDb
     .collection("users")
     .doc(userId!)
@@ -14,10 +15,14 @@ async function ChatToFilePage({ params: { id } }: { params: { id: string } }) {
     .get();
 
   const url = ref.data()?.dowloadUrl;
+  console.log(ref);
+  console.log(url);
   return (
     <div className="grid h-full overflow-hidden lg:grid-cols-5">
       <div className="col-span-5 overflow-y-auto lg:col-span-2"></div>
-      <div className="col-span-5 overflow-auto border-r-2 bg-gray-100 lg:-order-1 lg:col-span-3 lg:border-indigo-600"></div>
+      <div className="col-span-5 overflow-auto border-r-2 bg-gray-100 lg:-order-1 lg:col-span-3 lg:border-indigo-600">
+        <PdfView url={url} />
+      </div>
     </div>
   );
 }
